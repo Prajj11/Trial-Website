30898a9e42bf5c4b3e748cae77bc453c2046b5eb
Prajj11 <161217390+Prajj11@users.noreply.github.com>
2026-06-14 20:41:38 +0530

    chore: install project dependencies via npm update

--- animepahe-api--main/routes/playRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const PlayController = require('../controllers/playController');
-
-const router = express.Router();
-
-router.get('/play/:id', PlayController.getStreamingLinks);
-
-module.exports = router;
